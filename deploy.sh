56ea0cfa10035d8291d6f4c6a60c869fbad96be3
janluyleonardo <janluy.moreno@gmail.com>
2026-04-26 18:50:22 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Esperar un momento para asegurar que la DB esté lista (opcional)
 echo "Running migrations..."
-php artisan migrate:fresh --seed
+php artisan migrate:fresh
 
 echo "Starting Apache..."
 apache2-foreground
